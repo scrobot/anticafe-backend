ce3a7db2d5cba3c710ea7718a940373588dd1c50
Alex Scrobot <scrobot91@gmail.com>
2016-02-11 19:15:33 +0700

    events migrations

---
+++ database/migrations/2016_02_11_150658_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('excerpt')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamp('start_at');
+            $table->timestamp('end_at');
+            $table->string('cover')->nullable();
+            $table->string('photo')->nullable();
+            $table->text('price')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
