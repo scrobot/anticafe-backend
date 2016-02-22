e54fbc9d8df0e0e66503d17c1f24a460d77a9dfc
Alex Scrobot <scrobot91@gmail.com>
2016-02-22 16:08:59 +0700

    permissions roles

--- packages/helpers/roles/resources/views/permissions.blade.php
+++ packages/helpers/roles/resources/views/permissions.blade.php
@@ -16,11 +16,11 @@
     <h1>Набор правил для роли {{$role->name}}</h1>
     <form action="{{action('\Helpers\Roles\RoleController@postAttachWithRole', $role->id)}}" method="post">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        @foreach(app('modules') as $module)
-            <h3>{{$module['name']}}</h3>
+        @foreach(config('modules') as $module)
+            <h3>{{$module}}</h3>
             <div class="form-group">
                 @foreach($permissions as $perm)
-                    @if($module['name'] == $perm->module['name'])
+                    @if($module == $perm->group)
                         <div class="checkbox">
                             <label>
                                 <input type="checkbox" name="permissions[{{$perm->id}}]" value="{{$perm->id}}" {{$role->permissions->contains($perm->id) ? "checked" : ''}}> {{$perm->name}}
@@ -34,7 +34,7 @@
         <h3>Прочее</h3>
         <div class="form-group">
             @foreach($permissions as $perm)
-                @if(is_null($perm->module))
+                @if($perm->group == "Общее правило")
                     <div class="checkbox">
                         <label>
                             <input type="checkbox" name="permissions[{{$perm->id}}]" value="{{$perm->id}}" {{$role->permissions->contains($perm->id) ? "checked" : ''}}> {{$perm->name}}
