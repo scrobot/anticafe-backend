df8c6f083fd5d7e44e21f4b722ab4a4b9a8b234a
Alex Scrobot <scrobot91@gmail.com>
2016-02-23 11:07:56 +0800

    middlewares

---
+++ app/Http/Middleware/RuleRoles.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Anticafe\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RuleRoles
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        check_perm('roles.all');
+
+        return $next($request);
+    }
+}
